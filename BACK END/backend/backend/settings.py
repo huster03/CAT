4be12c33b8284c4764ed3e4462dd54d6f0446079
肖逍 <1912652748@qq.com>
2024-05-06 20:45:31 +0800

    连接数据库

--- BACK END/backend/backend/settings.py
+++ BACK END/backend/backend/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': 
+    { 
+        'ENGINE': 'django.db.backends.mysql',    # 数据库引擎
+        'NAME': 'test', # 数据库名称
+        'HOST': '127.0.0.1', # 数据库地址，本机 ip 地址 127.0.0.1 
+        'PORT': 3306, # 端口 
+        'USER': 'root',  # 数据库用户名
+        'PASSWORD': '', # 数据库密码
+    } 
 }
 
 

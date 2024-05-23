3bbbf662bd25d5a55bfd57fa35bc3062fb37067d
肖逍 <1912652748@qq.com>
2024-05-23 20:25:46 +0800

    修改前端代码

--- specialdata/urls.py
+++ specialdata/urls.py
@@ -1,9 +1,9 @@
 # urls.py
 from django.urls import path
-from .views import term_bank_interface, search_term_bank
+from .views import term_bank_list, search_term_bank
 
 urlpatterns = [
-    path('term_bank_interface/', term_bank_interface, name='term_bank_interface'),
+    path('term_bank_list/', term_bank_list, name='term_bank_list'),
     path('search_term_bank/', search_term_bank, name='search_term_bank'),
     # 可以添加更多的 URL 映射
 ]
--- specialdata/views.py
+++ specialdata/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import TermBankList, TermBankDetail
 # Create your views here.
-def term_bank_interface(request):
+def term_bank_list(request):
     # 获取术语库列表
     term_banks = TermBankList.objects.all()
 
     # 将术语库列表传递给模板
-    return render(request, 'term_bank_interface.html', {'term_banks': term_banks})
+    return render(request, 'term_bank.html', {'term_banks': term_banks})
 
 def search_term_bank(request):
     # 处理搜索逻辑

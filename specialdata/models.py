cacf54d6fb462e42e90c2266b34b27a2f6818cf4
肖逍 <1912652748@qq.com>
2024-05-23 19:14:49 +0800

    修改表

--- specialdata/models.py
+++ specialdata/models.py
@@ -1,8 +1,16 @@
 from __future__ import unicode_literals
 from django.db import models
 
-class Specialdatatable(models.Model):
-    objects = models.Manager()
+class TermBankList(models.Model):
+    source_language = models.TextField()
+    target_language = models.TextField()
+    term_bank = models.TextField()
+    number_of_terms = models.IntegerField()
+    user = models.ForeignKey('usersignin.User', on_delete=models.CASCADE)
+
+class TermBankDetail(models.Model):
+    term_bank_name = models.TextField()
     source_text = models.TextField()
     target_text = models.TextField()
+    resource = models.TextField()
     user = models.ForeignKey('usersignin.User', on_delete=models.CASCADE)

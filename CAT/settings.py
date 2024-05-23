041f8e837aa21e756552c6817d0e1e48064ca4ed
HUSTERandENDER <15272984770@163.com>
2024-05-23 20:33:38 +0800

    完善翻译界面

--- CAT/settings.py
+++ CAT/settings.py
@@ -131,11 +131,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'FRONT END' / 'CSS',
     BASE_DIR / 'FRONT END' / 'CSS' / 'general',
+    BASE_DIR / 'FRONT END',
 ]
 
 # Default primary key field type

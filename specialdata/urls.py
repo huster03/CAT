a12dfa5989bc801c9bfccd15ac3b706a7fa3729f
肖逍 <1912652748@qq.com>
2024-05-27 20:12:43 +0800

    添加从列表到详情的跳转

--- specialdata/urls.py
+++ specialdata/urls.py
@@ -1,9 +1,11 @@
 # urls.py
 from django.urls import path
 from .views import term_bank_list, search_term_bank
+from . import views
 
 urlpatterns = [
     path('term_bank_list/', term_bank_list, name='term_bank_list'),
     path('search_term_bank/', search_term_bank, name='search_term_bank'),
+    path('term_bank_detail/<int:term_bank_id>/', views.term_bank_detail, name='term_bank_detail'),
     # 可以添加更多的 URL 映射
 ]

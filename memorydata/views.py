6b22cf99613112cf93e9a0c14c027ff915070b4b
肖逍 <1912652748@qq.com>
2024-05-27 21:50:57 +0800

    添加新建功能

--- memorydata/views.py
+++ memorydata/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import MemoryBankList,MemoryBankDetail
 from django.core.paginator import Paginator
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.utils.decorators import method_decorator
+from datetime import datetime
 
 # Create your views here.
 def user_certre(request):
@@ -12,10 +17,7 @@ def memory_bank_list(request):
 
 def memory_bank_detail(request, memory_bank_id):
     memory_bank = memory_bank.objects.get(id=memory_bank_id)
-    try:
-        memory_bank_detail = MemoryBankDetail.objects.get(memory_bank=memory_bank)
-    except MemoryBankDetail.DoesNotExist:
-        memory_bank_detail = None
+    memory_bank_detail = MemoryBankDetail.objects.filter(memory_bank=memory_bank)
     paginator = Paginator(memory_bank_detail, 20)  # 在这里假设一页显示20条数据
     page = request.GET.get('page')
     memory_bank_page = paginator.get_page(page)
@@ -37,3 +39,29 @@ def addMemoryData(request, user_id):
         add_memory_target = request.POST.get('add_memo_target')
         addedMemoryData = MemoryBankDetail(source_text=add_memory_source, target_text=add_memory_target, user_id=user_id)
         addedMemoryData.save()
+        # 返回一个成功的消息或者重定向到另一个页面
+        return JsonResponse({'message': '记忆数据添加成功'}, status=201)  # 或者使用 redirect() 函数重定向到另一个页面
+
+@method_decorator(csrf_exempt, name='dispatch')
+def create_memory_bank(request):
+    if request.method == 'POST':
+        # 接收前端发送过来的数据
+        data = json.loads(request.body)
+        name = data.get('name')
+        source_language = data.get('source_language')
+        target_language = data.get('target_language')
+        try:
+            generated_date = datetime.now()
+
+            new_memory_bank = MemoryBankList(name=name, source_language=source_language, target_language=target_language, generated_date=generated_date)
+            new_memory_bank.save()
+            # 返回创建成功的消息
+            return JsonResponse({'message': '记忆库创建成功'}, status=201)
+        except Exception as e:
+            # 返回创建失败的消息
+            return JsonResponse({'error': '记忆库创建失败: {}'.format(str(e))}, status=500)
+        
+    else:
+        # 如果不是POST请求，返回错误消息
+        return JsonResponse({'error': '只支持POST请求'}, status=400)
+    

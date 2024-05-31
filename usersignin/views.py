8f07b6ec26ff1f5d14481f736dffcba3a1ce5918
HUSTERandENDER <15272984770@163.com>
2024-05-31 10:17:00 +0800

    usercentre

--- usersignin/views.py
+++ usersignin/views.py
@@ -1,16 +1,23 @@
-#coding=utf-8
-from django.shortcuts import render , redirect
-from django.http import HttpResponse
+# coding=utf-8
+import datetime
+import PyPDF2
+import datetime
 from django import forms
-from .models import User
-from memorydata.models import MemoryBankDetail , MemoryBankList
-from specialdata.models import TermBankList , TermBankItem
-import os
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from docx import Document
+from memorydata.models import MemoryBankDetail
+from specialdata.models import TermBankList
 from mainwds import translate
 from .models import TextTranslationPart
-from CAT import settings
-import datetime
-# Create your views here.
+from .models import User
+
+
+class UserForm(forms.Form):
+    username = forms.CharField(label='用户名', max_length=50)
+    password = forms.CharField(label='密码', widget=forms.PasswordInput())
+    email = forms.EmailField(label='邮箱')
+
 class UploadFileForm(forms.Form):
     file = forms.FileField()
 
@@ -18,15 +25,15 @@ def get_project_infos_for_user(username):
     return TextTranslationPart.objects.filter(username=username) \
                           .values('project_name', 'created_at') \
                           .distinct()
+
+
 def regist(request):
     if request.method == 'POST':
         username = request.POST.get("username")
         password = request.POST.get("password")
         user = User.objects.create(username=username,password=password)
-        termbanklist = TermBankList.objects.create(user = user)
-        memorybanklist = MemoryBankList.objects.create(user = user)
-        MemoryBankDetail.objects.create(user = user , memory_bank = memorybanklist)
-        TermBankItem.objects.create(user = user , term_bank = termbanklist)
+        MemoryBankDetail.objects.create(user = user)
+        TermBankList.objects.create(user = user)
         user.save()
         return redirect('/login')
     return render(request,'register.html')
@@ -44,29 +51,51 @@ def login(request):
     return render(request,'login.html')
 def user_centre(request):
     username = request.session.get('username')
+    form = UploadFileForm()
 
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             uploaded_file = request.FILES['file']
-            file_path = uploaded_file.name
-            path = os.path.join(settings.MEDIA_ROOT, file_path)
-            with open(path, 'wb') as destination:
+            with open('media/copy_file.txt', 'wb+') as destination:
                 for chunk in uploaded_file.chunks():
                     destination.write(chunk)
+
+            file_extension = uploaded_file.name.split('.')[-1]
+
+            # 根据文件扩展名进行不同的处理
+            if file_extension == 'txt':
+                # 打开保存的txt文件，并读取内容
+                with open('media/copy_file.txt', 'r', encoding='utf-8') as file:
+                    lines = file.readlines()
+
+            elif file_extension == 'docx':
+                # 使用python-docx库读取docx文件内容
+                doc = Document('media/copy_file.txt')
+                lines = [paragraph.text for paragraph in doc.paragraphs]
+
+            elif file_extension == 'pdf':
+                # 使用PyPDF2库读取PDF文件内容
+                with open('media/copy_file.txt', 'rb') as file:
+                    pdf_reader = PyPDF2.PdfReader(file)
+                    lines = []
+                    for page in pdf_reader.pages:
+                        lines.append(page.extract_text())
+            with open('media/last_copy.txt', 'w', encoding='utf-8') as file:
+                file.writelines(lines)
+            path = 'media/last_copy.txt'
             source, target = translate.trans(path)
+            # 按索引分段保存到数据库
             for i in range(len(source)):
                 TextTranslationPart.objects.create(
                     username=username,
                     part_index=i,
+                    project_name = uploaded_file.name,
                     source_text=source[i],
                     target_text=target[i],
-                    project_name = uploaded_file.name,
                     create_at = datetime.datetime.now()
                 )
             return redirect('/main_display/')
-    else:
-        form = UploadFileForm()
 
     project_infos = get_project_infos_for_user(username)
 

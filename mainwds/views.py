d0edeb588c6170dbb6402ee42f8594dad13e19f6
huster03 <2179645520@qq.com>
2024-06-06 17:24:13 +0800

    异步更新

--- mainwds/views.py
+++ mainwds/views.py
@@ -2,8 +2,10 @@ from django import forms
 from django.http import JsonResponse
 from django.shortcuts import render
 from usersignin.models import User
-from usersignin.models import  TextTranslationPart
+from usersignin.models import TextTranslationPart
 import json
+from .translation import connect
+from django.http import JsonResponse
 
 context = {}
 
@@ -11,8 +13,9 @@ context = {}
 class UploadFileForm(forms.Form):
     file = forms.FileField()
 
-def get_translation_data(username , project_name):
-    parts = TextTranslationPart.objects.filter(username=username,project_name = project_name).order_by('part_index')
+
+def get_translation_data(username, project_name):
+    parts = TextTranslationPart.objects.filter(username=username, project_name=project_name).order_by('part_index')
     if parts:
         source_text_list = [part.source_text for part in parts]
         target_text_list = [part.target_text for part in parts]
@@ -20,29 +23,41 @@ def get_translation_data(username , project_name):
     else:
         return None, None
 
-def updateMysqldata(request,username , project_name):
+
+def updateMysqldata(request, username, project_name):
     if request.META.get('CONTENT_TYPE') == 'application/json':
         translated_content = json.loads(request.body)
-        parts = TextTranslationPart.objects.filter(username=username,project_name = project_name)
+        parts = TextTranslationPart.objects.filter(username=username, project_name=project_name)
         for i, part in enumerate(parts):
             part.target_text = translated_content[i]
             part.save()
     else:
         pass
-def display_translation(request, username ,project_name):
+
+
+def display_translation(request, username, project_name):
     form = UploadFileForm()
-    source_text_lists, target_text_lists = get_translation_data(username , project_name)
+    source_text_lists, target_text_lists = get_translation_data(username, project_name)
     source_text_list = json.dumps(source_text_lists)
     target_text_list = json.dumps(target_text_lists)
     return {'form': form, 'username': username, 'source_text_list': source_text_list,
             'target_text_list': target_text_list}
 
 
-
-
-
-def main_display(request,project_name):
+def main_display(request, project_name):
     username = request.session.get('username')
-    updateMysqldata(request,username , project_name)
-    context.update(display_translation(request, username,project_name))
+    updateMysqldata(request, username, project_name)
+    display_translation(request, username, project_name)
+    context.update(display_translation(request, username, project_name))
     return render(request, 'main_display.html', context)
+
+
+def translate(request):
+    if request.method == 'POST':
+        word = request.POST.get('word')
+        response = connect(word)
+        data = json.loads(response)
+        translation = data['translation'][0]
+        print('1')
+        return JsonResponse({'result': translation})
+    return JsonResponse({'error': 'Invalid request method'})
--- memorydata/views.py
+++ memorydata/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import MemoryBankList,MemoryBankDetail
+from .models import MemoryBankList, MemoryBankDetail
 from django.core.paginator import Paginator
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -7,13 +7,16 @@ import json
 from django.utils.decorators import method_decorator
 from datetime import datetime
 
+
 # Create your views here.
 def user_certre(request):
     return render(request, 'user_centre.html')
 
+
 def memory_bank_list(request):
     memory_banks = MemoryBankList.objects.all()
-    return render(request, 'memory_bank.html', {'memory_banks': memory_banks})
+    return render(request, 'memo_bank.html', {'memory_banks': memory_banks})
+
 
 def memory_bank_detail(request, memory_bank_id):
     memory_bank = MemoryBankDetail.objects.get(id=memory_bank_id)
@@ -22,7 +25,8 @@ def memory_bank_detail(request, memory_bank_id):
     page = request.GET.get('page')
     memory_bank_page = paginator.get_page(page)
 
-    return render(request, 'memory_bank_detail.html', {'memory_bank': memory_bank, 'memory_bank_detail': memory_bank_detail, 'memory_bank_page': memory_bank_page})
+    return render(request, 'memo_detail.html', {'memory_bank': memory_bank, 'memory_bank_detail': memory_bank_detail,
+                                                'memory_bank_page': memory_bank_page})
 
 
 def searchMemoryData(request):
@@ -33,14 +37,17 @@ def searchMemoryData(request):
 
     return {}
 
+
 def addMemoryData(request, user_id):
     if request.method == "POST" and 'add_memory_source' in request.POST:
         add_memory_source = request.POST.get('add_memo_source')
         add_memory_target = request.POST.get('add_memo_target')
-        addedMemoryData = MemoryBankDetail(source_text=add_memory_source, target_text=add_memory_target, user_id=user_id)
+        addedMemoryData = MemoryBankDetail(source_text=add_memory_source, target_text=add_memory_target,
+                                           user_id=user_id)
         addedMemoryData.save()
         # 返回一个成功的消息或者重定向到另一个页面
         return JsonResponse({'message': '记忆数据添加成功'}, status=201)  # 或者使用 redirect() 函数重定向到另一个页面
+
 
 @method_decorator(csrf_exempt, name='dispatch')
 def create_memory_bank(request):
@@ -53,15 +60,15 @@ def create_memory_bank(request):
         try:
             generated_date = datetime.now()
 
-            new_memory_bank = MemoryBankList(name=name, source_language=source_language, target_language=target_language, generated_date=generated_date)
+            new_memory_bank = MemoryBankList(name=name, source_language=source_language,
+                                             target_language=target_language, generated_date=generated_date)
             new_memory_bank.save()
             # 返回创建成功的消息
             return JsonResponse({'message': '记忆库创建成功'}, status=201)
         except Exception as e:
             # 返回创建失败的消息
             return JsonResponse({'error': '记忆库创建失败: {}'.format(str(e))}, status=500)
-        
+
     else:
         # 如果不是POST请求，返回错误消息
         return JsonResponse({'error': '只支持POST请求'}, status=400)
-    
